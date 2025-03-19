f880c0b8e64a904b09f95a7379d6a8639606f841
vijaysingh966 <vijaysinghsisodiya966@gmail.com>
2025-03-20 02:28:05 +0530

    change all things

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -12,7 +12,7 @@ const sampleImages = [
   {
     id: 1,
     title: 'Final Match Celebration',
-    imageUrl: 'src/images/virat4.jpg',
+    imageUrl: 'src/pages/images/virat4.jpg',
     isPremium: true,
     price: 5.99,
     category: 'Team Celebrations',
@@ -20,7 +20,7 @@ const sampleImages = [
   {
     id: 2,
     title: 'Perfect Batting Stance',
-    imageUrl: '/images/viratrohit1.jpg',
+    imageUrl: 'src\pages\images\viratrohit1.jpg',
     isPremium: true,
     price: 4.99,
     category: 'Match Highlights',
